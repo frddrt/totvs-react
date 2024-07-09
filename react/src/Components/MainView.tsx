6212c06541ab4d24c5fd2146f9643aa43fd89ecc
Frederico Ferracini Duarte <frederico@ferracini.net.br>
2024-07-09 17:39:15 -0300

    Conn by totvstec lib.

--- react/src/Components/MainView.tsx
+++ react/src/Components/MainView.tsx
@@ -3,13 +3,29 @@
  * @since 2023-06-05 15:09:06
  */
 
+import { useEffect } from "react"
 import { Container } from "react-bootstrap"
 import { useRecoilValue } from "recoil"
 import { If } from "../Library/If"
 import { AppState } from "../Store/atom"
 
+declare global {
+	interface Window {
+		observeProtheus: any
+	}
+}
+
+window.observeProtheus = window.observeProtheus || {}
+
 const MainView = () => {
 	const appState = useRecoilValue(AppState)
+
+	useEffect(() => {
+		window.observeProtheus = (key: string, value: any) => {
+			if (key === "<YOUR_KEY>") {
+			}
+		}
+	}, [])
 
 	return (
 		<Container className='p-2'>
---
+++ react/src/global.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Frederico Ferracini Duarte
+ * @since 2024-07-09 17:38:08
+ */
+
+module.exports = {}
+
+declare const totvstec: {
+	jsToAdvpl: (key: string, value?: any) => void
+	advplToJs: (key: string, value?: any) => void
+}
